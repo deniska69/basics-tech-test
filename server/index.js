2cf95a54b2ff4dd5d2a80b52348684ade44e9f83
Deniska <47465987+deniska69@users.noreply.github.com>
2022-06-21 05:35:09 +0700

    Разработана основа api сервера, установлено соденинение с бд

--- server/index.js
+++ server/index.js
@@ -1,1 +1,33 @@
-console.log("server started");
+const express = require('express');
+const mongoose = require('mongoose');
+const config = require('config');
+
+const corsMiddleware = require('./middleware/cors.middleware');
+const PORT = config.get('serverPort');
+const URL_DB = config.get('dbUrl');
+
+const app = express();
+app.use(corsMiddleware);
+app.use(express.json());
+app.use(express.static('static'));
+
+const start = async () => {
+  try {
+    await mongoose.connect(URL_DB);
+
+    app.listen(PORT, () => {
+      console.log("\nBack-Сервер 'basics_tech_test' запущен [", new Date().toLocaleString('ru-RU', { timeZone: 'Europe/Moscow' }), '(МСК)]\nПорт: ', PORT, ' \n');
+
+      var twirlTimer = (function () {
+        var P = ['\\', '|', '/', '-'];
+        var x = 0;
+        return setInterval(function () {
+          process.stdout.write('\r' + P[x++]);
+          x &= 3;
+        }, 250);
+      })();
+    });
+  } catch (e) {}
+};
+
+start();
