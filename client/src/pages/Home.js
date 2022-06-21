c1f18e1cd1f852f0098b7b382aa20e785956b252
Deniska <47465987+deniska69@users.noreply.github.com>
2022-06-21 13:08:42 +0700

    Добавлена вёрстка формы авторизации, настроено переключение между формами

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -7,7 +7,6 @@ import {
   InputAdornment,
   IconButton,
   Card,
-  CardActions,
   CardContent,
   Select,
   MenuItem,
@@ -16,16 +15,36 @@ import {
   Box,
   Button,
 } from '@mui/material';
+import PropTypes from 'prop-types';
 import { Visibility, VisibilityOff } from '@mui/icons-material';
-import { Link } from 'react-router-dom';
+//import { Link } from 'react-router-dom';
 import './Home.css';
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div role="tabpanel" hidden={value !== index} id={`simple-tabpanel-${index}`} aria-labelledby={`simple-tab-${index}`} {...other}>
+      {value === index && <Box sx={{ p: 1 }}>{children}</Box>}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
 
 export default function Home() {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  // eslint-disable-next-line
   const [dateBirth, setDateBirth] = useState('');
   const [gender, setGender] = useState('');
+
+  const [tabIndex, setTabIndex] = useState(0);
 
   const [configInputPassword, setConfigInputPassword] = useState({
     amount: '',
@@ -46,79 +65,149 @@ export default function Home() {
     event.preventDefault();
   };
 
+  const selectTab = () => {
+    if (tabIndex === 0) {
+      setTabIndex(1);
+    } else if (tabIndex === 1) {
+      setTabIndex(0);
+    }
+  };
+
   return (
     <Container maxWidth="sm">
-      <Card spacing={2} width="sm" sx={{ p: { xs: 2, md: 3 } }}>
+      <Card spacing={2} width="sm" sx={{ xs: 2, md: 3 }}>
         <CardContent>
-          <Grid container spacing={3}>
-            <Grid item xs={12}>
-              <Typography variant="h5" align="center">
-                Регистрация
-              </Typography>
-            </Grid>
+          <TabPanel value={tabIndex} index={0}>
+            <Grid container spacing={3}>
+              <Grid item xs={12}>
+                <Typography variant="h5" align="center">
+                  Регистрация
+                </Typography>
+              </Grid>
 
-            <Grid item xs={12}>
-              <FormControl fullWidth variant="outlined">
-                <InputLabel htmlFor="outlined-adornment-name">Имя</InputLabel>
-                <OutlinedInput id="outlined-adornment-name" value={name} onChange={e => setName(e.target.value)} label="Имя" />
-              </FormControl>
-            </Grid>
+              <Grid item xs={12}>
+                <FormControl fullWidth variant="outlined">
+                  <InputLabel htmlFor="outlined-adornment-name">Имя</InputLabel>
+                  <OutlinedInput id="outlined-adornment-name" value={name} onChange={e => setName(e.target.value)} label="Имя" />
+                </FormControl>
+              </Grid>
 
-            <Grid item xs={12}>
-              <FormControl fullWidth variant="outlined">
-                <InputLabel htmlFor="outlined-adornment-email">Email:</InputLabel>
-                <OutlinedInput id="outlined-adornment-email" value={email} onChange={e => setEmail(e.target.value)} label="Имя" />
-              </FormControl>
-            </Grid>
+              <Grid item xs={12}>
+                <FormControl fullWidth variant="outlined">
+                  <InputLabel htmlFor="outlined-adornment-email">Email:</InputLabel>
+                  <OutlinedInput id="outlined-adornment-email" value={email} onChange={e => setEmail(e.target.value)} label="Имя" />
+                </FormControl>
+              </Grid>
 
-            <Grid item xs={12}>
-              <FormControl fullWidth variant="outlined">
-                <InputLabel htmlFor="outlined-adornment-password">Пароль</InputLabel>
-                <OutlinedInput
-                  id="outlined-adornment-password"
-                  type={configInputPassword.showPassword ? 'text' : 'password'}
-                  value={password}
-                  onChange={e => setPassword(e.target.value)}
-                  inputProps={{
-                    autoComplete: 'new-password',
-                  }}
-                  endAdornment={
-                    <InputAdornment position="end">
-                      <IconButton aria-label="toggle password visibility" onClick={handleClickShowPassword} onMouseDown={handleMouseDownPassword} edge="end">
-                        {configInputPassword.showPassword ? <VisibilityOff /> : <Visibility />}
-                      </IconButton>
-                    </InputAdornment>
-                  }
-                  label="Пароль"
-                />
-              </FormControl>
-            </Grid>
+              <Grid item xs={12}>
+                <FormControl fullWidth variant="outlined">
+                  <InputLabel htmlFor="outlined-adornment-password">Пароль</InputLabel>
+                  <OutlinedInput
+                    id="outlined-adornment-password"
+                    type={configInputPassword.showPassword ? 'text' : 'password'}
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
+                    inputProps={{
+                      autoComplete: 'new-password',
+                    }}
+                    endAdornment={
+                      <InputAdornment position="end">
+                        <IconButton aria-label="toggle password visibility" onClick={handleClickShowPassword} onMouseDown={handleMouseDownPassword} edge="end">
+                          {configInputPassword.showPassword ? <VisibilityOff /> : <Visibility />}
+                        </IconButton>
+                      </InputAdornment>
+                    }
+                    label="Пароль"
+                  />
+                </FormControl>
+              </Grid>
 
-            <Grid item xs={12}>
-              <FormControl fullWidth>
-                <InputLabel id="demo-simple-select-label">Пол:</InputLabel>
-                <Select labelId="demo-simple-select-label" id="demo-simple-select" value={gender} label="Пол" onChange={e => setGender(e.target.value)}>
-                  <MenuItem value={'М'}>М</MenuItem>
-                  <MenuItem value={'Ж'}>Ж</MenuItem>
-                </Select>
-              </FormControl>
-            </Grid>
+              <Grid item xs={12}>
+                <FormControl fullWidth>
+                  <InputLabel id="demo-simple-select-label">Пол:</InputLabel>
+                  <Select labelId="demo-simple-select-label" id="demo-simple-select" value={gender} label="Пол" onChange={e => setGender(e.target.value)}>
+                    <MenuItem value={'М'}>М</MenuItem>
+                    <MenuItem value={'Ж'}>Ж</MenuItem>
+                  </Select>
+                </FormControl>
+              </Grid>
 
-            <Grid item xs={12} display={'flex'} justifyContent={'center'}>
-              <Button variant="contained">Зарегистрироваться</Button>
-            </Grid>
+              <Grid item xs={12} display={'flex'} justifyContent={'center'}>
+                <Button variant="contained" size="large">
+                  Зарегистрироваться
+                </Button>
+              </Grid>
 
-            <Grid item xs={12}>
-              <Box display={'flex'} justifyContent={'center'}>
-                Уже есть аккаунт?
-                <Link className="linkToAccountPage" to="/account">
-                  {'    '}Войти.
-                </Link>
-              </Box>
+              <Grid item xs={12}>
+                <Box display={'flex'} justifyContent={'center'} alignItems={'center'}>
+                  <Typography color={'#556cd6'} fontSize={'0.875rem;'} marginRight={'0.5rem'}>
+                    УЖЕ ЕСТЬ АККАУНТ?
+                  </Typography>
+                  <Button variant="outlined" size="medium" onClick={selectTab}>
+                    Войти
+                  </Button>
+                </Box>
+              </Grid>
             </Grid>
-          </Grid>
+          </TabPanel>
+
+          <TabPanel value={tabIndex} index={1}>
+            <Grid container spacing={3}>
+              <Grid item xs={12}>
+                <Typography variant="h5" align="center">
+                  Авторизация
+                </Typography>
+              </Grid>
+
+              <Grid item xs={12}>
+                <FormControl fullWidth variant="outlined">
+                  <InputLabel htmlFor="outlined-adornment-email">Email:</InputLabel>
+                  <OutlinedInput id="outlined-adornment-email" value={email} onChange={e => setEmail(e.target.value)} label="Имя" />
+                </FormControl>
+              </Grid>
+
+              <Grid item xs={12}>
+                <FormControl fullWidth variant="outlined">
+                  <InputLabel htmlFor="outlined-adornment-password">Пароль</InputLabel>
+                  <OutlinedInput
+                    id="outlined-adornment-password"
+                    type={configInputPassword.showPassword ? 'text' : 'password'}
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
+                    inputProps={{
+                      autoComplete: 'new-password',
+                    }}
+                    endAdornment={
+                      <InputAdornment position="end">
+                        <IconButton aria-label="toggle password visibility" onClick={handleClickShowPassword} onMouseDown={handleMouseDownPassword} edge="end">
+                          {configInputPassword.showPassword ? <VisibilityOff /> : <Visibility />}
+                        </IconButton>
+                      </InputAdornment>
+                    }
+                    label="Пароль"
+                  />
+                </FormControl>
+              </Grid>
+
+              <Grid item xs={12} display={'flex'} justifyContent={'center'}>
+                <Button variant="contained" size="large">
+                  Войти
+                </Button>
+              </Grid>
+
+              <Grid item xs={12}>
+                <Box display={'flex'} justifyContent={'center'} alignItems={'center'}>
+                  <Typography color={'#556cd6'} fontSize={'0.875rem;'} marginRight={'0.5rem'}>
+                    НЕТ АККАУНТА?
+                  </Typography>
+                  <Button variant="outlined" size="medium" onClick={selectTab}>
+                    Зарегистрироваться
+                  </Button>
+                </Box>
+              </Grid>
+            </Grid>
+          </TabPanel>
         </CardContent>
-        <CardActions></CardActions>
       </Card>
     </Container>
   );
