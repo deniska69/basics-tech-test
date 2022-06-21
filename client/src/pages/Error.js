b25836e5798665f382e359b9fbe953731d82d33d
Deniska <47465987+deniska69@users.noreply.github.com>
2022-06-21 21:18:32 +0700

    Обновлена вёрстка страницы 404

--- client/src/pages/Error.js
+++ client/src/pages/Error.js
@@ -1,3 +1,17 @@
+import { Container, Card, CardContent, Typography } from '@mui/material';
+import { Link } from 'react-router-dom';
+
 export default function Error() {
-  return <div>Ошибка 404 - такой страницы не существует! :с</div>;
+  return (
+    <Container maxWidth="sm">
+      <Card width="sm" sx={{ xs: 2, md: 3 }}>
+        <CardContent sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+          <Typography variant="h5" align="center" marginBottom={'3vh'}>
+            Ошибка 404 - такой страницы не существует!
+          </Typography>
+          <Link to="/">Вернуться на главную страницу</Link>
+        </CardContent>
+      </Card>
+    </Container>
+  );
 }
